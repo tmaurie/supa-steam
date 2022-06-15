38af0ace903eb1bf8bc4830aa050fa41af8280c9
tmaurie <thomas.maurier91@gmail.com>
2022-06-16 00:17:25 +0200

    [FEATURE] GameItem.js

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,5 +1,20 @@
+import { Box, Flex, Heading } from '@chakra-ui/react';
+
 export function GameItem(game) {
   return (
-    <li> {game.title}</li>
+    <Box
+         key={game.id}
+         mr={{ base: "25px", lg: "68px" }}
+         ml={{ base: "25px", lg: "68px" }}
+         mt={{ base: "0px", lg: "20px" }}
+         mb="20px"
+         borderRadius="30px"
+         bg="#101720"
+         w='100%' p={4} color='white'>
+      <Flex>
+        <img src={game.image}  alt="test"/>
+        <Heading>{game.title}</Heading>
+      </Flex>
+    </Box>
   );
 }

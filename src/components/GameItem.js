93ee62fb8c09e4695b3aa27f70f7068525cf8148
tmaurie <thomas.maurier91@gmail.com>
2022-07-29 23:04:24 +0200

    [FEATURE] Added tags

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,20 +1,29 @@
-import { Box, Flex, Heading, Image } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Tag } from '@chakra-ui/react';
 
 export function GameItem(game) {
   return (
     <Box
-         key={game.id}
-         mr={{ base: "25px", lg: "68px" }}
-         ml={{ base: "25px", lg: "68px" }}
-         mt={{ base: "0px", lg: "20px" }}
-         mb="20px"
-         borderRadius="30px"
-         bg="#101720"
-         w='100%' p={4} color='white'>
-      <Flex>
-        <Image src={game.image} borderRadius="15px" alt="test"/>
-        <Heading>{game.title}</Heading>
-      </Flex>
+      key={game.id}
+      mr={{ base: '25px', lg: '68px' }}
+      ml={{ base: '25px', lg: '68px' }}
+      mt={{ base: '0px', lg: '20px' }}
+      mb='20px'
+      borderRadius='30px'
+      bg='#101720'
+      w='100%' p={4} color='white'>
+      <Flex flexDirection={{ base: 'column', lg: 'row' }}
+            justifyContent='left'>
+        <Image src={game.image} borderRadius='15px' alt='test' />
+        <Flex wrap='wrap' mt='12px' flexDirection="column">
+          <Heading>{game.title}</Heading>
+
+          <ul>
+            {game.tags.map(item => (
+              <Tag>{item}</Tag>
+            ))}
+          </ul>
+          </Flex>
+        </Flex>
     </Box>
-  );
+);
 }

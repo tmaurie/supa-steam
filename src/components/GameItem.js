b5b6ba9e4a360ab67ae8bb88dfba6144ad91ba1e
tmaurie <thomas.maurier91@gmail.com>
2022-11-10 17:19:03 +0100

    [STYLE] added styling to titles

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,6 +1,7 @@
-import { Box, Flex, Heading, Image, Tag } from '@chakra-ui/react';
+import { Box, Flex, Heading, HStack, Image, Tag } from '@chakra-ui/react';
 
 export function GameItem(game) {
+
   return (
     <Box
       key={game.id}
@@ -12,18 +13,49 @@ export function GameItem(game) {
       bg='#101720'
       w='100%' p={4} color='white'>
       <Flex flexDirection={{ base: 'column', lg: 'row' }}
-            justifyContent='left'>
+            justifyContent='space-between' gap={4}>
         <Image src={game.image} borderRadius='15px' alt='test' />
-        <Flex wrap='wrap' mt='12px' flexDirection="column">
-          <Heading>{game.title}</Heading>
 
-          <ul>
-            {game.tags.map(item => (
-              <Tag>{item}</Tag>
-            ))}
-          </ul>
-          </Flex>
+        <Flex
+          flexDirection={{ base: 'column', lg: 'row' }}
+          justifyContent='space-between'
+          pl={{ base: '20px', lg: '51px' }}
+          pt={{ base: '20px', lg: '53px' }}
+          pb={{ base: '20px', lg: '34px' }}
+          pr={{ base: '20px', lg: '34px' }}
+          w='100%'
+          pos='relative'
+        >
+          <Box>
+            <Heading style={{ overflowWrap: 'break-word' }} fontSize='28px' fontWeight='semibold' lineHeight='26.77px'>
+              {game.title}
+            </Heading>
+            <HStack spacing={2}>
+              {game.tags.map(tag => (
+                <Tag>{tag}</Tag>
+              ))}
+            </HStack>
+            <Box
+              mt='25px'
+              h='10px'
+              w='127px'
+              borderRadius='20px'
+              bg='#214B6B'
+            ></Box>
+          </Box>
+
         </Flex>
+        <Box
+          d='flex'
+          justifyContent='space-between'
+          flexDirection={{ base: 'row', lg: 'column' }}
+          alignItems='flex-end'
+        >
+          <Heading fontSize='40px' fontWeight='bold'>
+            {'$' + game.price.toFixed(2)}
+          </Heading>
+        </Box>
+      </Flex>
     </Box>
-);
+  );
 }
